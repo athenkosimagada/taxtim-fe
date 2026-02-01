17ae8f73c6cd17eb22e7319427df333c71128cf0
Athenkosi Magada <magadaathenkosi@gmail.com>
2026-02-01 19:44:47 +0200

    feat: update project structure with new components and styles, add environment file support, and enhance configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/transactions`)
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>{apiUrl}</p>
+      <header>Header</header>
+
+      <main>
+        <ul>
+          {transactions.map((transaction) => (
+            <li key={transaction.id}>
+              {transaction.type} {transaction.amount}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
